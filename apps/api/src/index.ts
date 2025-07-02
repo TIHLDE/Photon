5840227fa02b7dc289bc74f2f36b2fe0d75ff91b
Conrad Osvik <conrad.tinius.osvik@gmail.com>
2025-07-02 05:59:35 +0200

    feat: setup scalar

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -4,42 +4,64 @@ import { cors } from "hono/cors";
 import { auth } from "./lib/auth";
 import { session } from "./middleware/session";
 import router from "./routes/example";
+import { openAPISpecs } from "hono-openapi";
+import { Scalar } from "@scalar/hono-api-reference";
 
-const app = new Hono();
+const app = new Hono()
+    .use(
+        "/api/auth/**",
+        cors({
+            origin: "http://localhost:3000",
+            allowHeaders: ["Content-Type", "Authorization"],
+            allowMethods: ["POST", "GET", "OPTIONS"],
+            exposeHeaders: ["Content-Length"],
+            maxAge: 600,
+            credentials: true,
+        }),
+    )
+    .on(["POST", "GET"], "/api/auth/*", (c) => {
+        return auth.handler(c.req.raw);
+    })
+    .get("/", (c) => {
+        return c.text("Hello Hono!");
+    })
+    .get("/session", session, (c) => {
+        const session = c.get("session");
+        const user = c.get("user");
 
-app.use(
-    "/api/auth/**",
-    cors({
-        origin: "http://localhost:3000",
-        allowHeaders: ["Content-Type", "Authorization"],
-        allowMethods: ["POST", "GET", "OPTIONS"],
-        exposeHeaders: ["Content-Length"],
-        maxAge: 600,
-        credentials: true,
+        if (!user) return c.body(null, 401);
+
+        return c.json({
+            session,
+            user,
+        });
+    })
+    .route("/", router);
+
+app.get(
+    "/openapi",
+    openAPISpecs(app, {
+        documentation: {
+            info: {
+                title: "Hono API",
+                version: "1.0.0",
+                description: "Greeting API",
+            },
+            servers: [
+                {
+                    url: "http://localhost:4000",
+                    description: "Local Server",
+                },
+            ],
+        },
+    }),
+).get(
+    "/docs",
+    Scalar({
+        theme: "saturn",
+        url: "/openapi",
     }),
 );
-
-app.on(["POST", "GET"], "/api/auth/*", (c) => {
-    return auth.handler(c.req.raw);
-});
-
-app.get("/", (c) => {
-    return c.text("Hello Hono!");
-});
-
-app.get("/session", session, (c) => {
-    const session = c.get("session");
-    const user = c.get("user");
-
-    if (!user) return c.body(null, 401);
-
-    return c.json({
-        session,
-        user,
-    });
-});
-
-app.route("/", router);
 
 serve(
     {
