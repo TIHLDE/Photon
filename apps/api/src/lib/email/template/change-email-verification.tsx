63c017ff68090fde48af169d2bac4ab5c400f53a
Mori <anders.morille@gmail.com>
2025-09-07 00:54:18 +0200

    Cooked up nice transactional emails

--- apps/api/src/lib/email/template/change-email-verification.tsx
+++ apps/api/src/lib/email/template/change-email-verification.tsx
@@ -1,21 +1,164 @@
-import { Button, Html } from "@react-email/components";
 import React from "react";
+import {
+    Body,
+    Button,
+    Container,
+    Head,
+    Heading,
+    Html,
+    Img,
+    Link,
+    Section,
+    Text,
+} from "@react-email/components";
+import { env } from "../../env";
 
 interface ChangeEmailVerificationProps {
-  url: string;
+    url: string;
 }
 
-export const ChangeEmailVerification = ({url} : ChangeEmailVerificationProps) => {
-  return (
-    <Html>
-      <Button
-        href={url}
-        style={{ background: "#000", color: "#fff", padding: "12px 20px" }}
-      >
-      Trykk her for å verifisere din nye e-postadresse
-      </Button>
-    </Html>
-  );
-}
+export const ChangeEmailVerification = ({
+    url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+}: ChangeEmailVerificationProps) => {
+    return (
+        <Html>
+            <Head />
+            <Body style={main}>
+                <Container style={container}>
+                    <Img
+                        src={`${env.BASE_URL}/static/logomark.jpeg`}
+                        width="100"
+                        height="100"
+                        alt="TIHLDE Logomark"
+                        style={logo}
+                    />
+                    <Heading style={secondary}>
+                        Trygg på knappen nedenfor for å bekrefte din nye
+                        e-postadresse
+                    </Heading>
+                    <Button href={url} style={button}>
+                        Bekreft e-post
+                    </Button>
+                    <Text style={paragraph}>
+                        Forventet du ikke denne e-posten?
+                    </Text>
+                    <Text style={paragraph}>
+                        Du kan trygt ignorere denne e-posten hvis du ikke prøvde
+                        å logge inn
+                    </Text>
+                </Container>
+                <Text style={footer}>Levert av INDEX</Text>
+            </Body>
+        </Html>
+    );
+};
 
 export default ChangeEmailVerification;
+
+const main = {
+    backgroundColor: "#ffffff",
+    fontFamily: "HelveticaNeue,Helvetica,Arial,sans-serif",
+};
+
+const container = {
+    backgroundColor: "#ffffff",
+    border: "1px solid #eee",
+    borderRadius: "5px",
+    boxShadow: "0 5px 10px rgba(20,50,70,.2)",
+    marginTop: "20px",
+    maxWidth: "360px",
+    margin: "0 auto",
+    padding: "68px 0 130px",
+};
+
+const logo = {
+    margin: "0 auto",
+    marginBottom: "24px",
+};
+
+const tertiary = {
+    color: "#0a85ea",
+    fontSize: "11px",
+    fontWeight: 700,
+    fontFamily: "HelveticaNeue,Helvetica,Arial,sans-serif",
+    height: "16px",
+    letterSpacing: "0",
+    lineHeight: "16px",
+    margin: "16px 8px 8px 8px",
+    textTransform: "uppercase" as const,
+    textAlign: "center" as const,
+};
+
+const secondary = {
+    color: "#000",
+    display: "inline-block",
+    fontFamily: "HelveticaNeue-Medium,Helvetica,Arial,sans-serif",
+    fontSize: "20px",
+    fontWeight: 500,
+    lineHeight: "24px",
+    marginBottom: "0",
+    marginTop: "0",
+    width: "100%",
+    textAlign: "center" as const,
+};
+
+const codeContainer = {
+    background: "rgba(0,0,0,.05)",
+    borderRadius: "4px",
+    margin: "16px auto 14px",
+    verticalAlign: "middle",
+    width: "280px",
+};
+
+const code = {
+    color: "#000",
+    fontFamily: "HelveticaNeue-Bold",
+    fontSize: "32px",
+    fontWeight: 700,
+    letterSpacing: "6px",
+    lineHeight: "40px",
+    paddingBottom: "8px",
+    paddingTop: "8px",
+    margin: "0 auto",
+    display: "block",
+    textAlign: "center" as const,
+};
+
+const paragraph = {
+    color: "#444",
+    fontSize: "15px",
+    fontFamily: "HelveticaNeue,Helvetica,Arial,sans-serif",
+    letterSpacing: "0",
+    lineHeight: "23px",
+    padding: "0 40px",
+    margin: "0",
+    marginTop: "10px",
+    textAlign: "center" as const,
+};
+
+const footer = {
+    color: "#000",
+    fontSize: "12px",
+    fontWeight: 800,
+    letterSpacing: "0",
+    lineHeight: "23px",
+    margin: "0",
+    marginTop: "20px",
+    fontFamily: "HelveticaNeue,Helvetica,Arial,sans-serif",
+    textAlign: "center" as const,
+    textTransform: "uppercase" as const,
+};
+
+const button = {
+    backgroundColor: "#007ee6",
+    borderRadius: "4px",
+    color: "#fff",
+    fontFamily: "'Open Sans', 'Helvetica Neue', Arial",
+    fontSize: "15px",
+    textDecoration: "none",
+    textAlign: "center" as const,
+    display: "block",
+    width: "210px",
+    margin: "20px auto 0",
+    padding: "14px 7px",
+};
--- apps/api/src/lib/email/template/otp-sign-in.tsx
+++ apps/api/src/lib/email/template/otp-sign-in.tsx
@@ -1,16 +1,147 @@
-import { Button, Html } from "@react-email/components";
 import React from "react";
+import {
+    Body,
+    Container,
+    Head,
+    Heading,
+    Html,
+    Img,
+    Link,
+    Section,
+    Text,
+} from "@react-email/components";
+import { env } from "../../env";
 
 interface OtpSignInProps {
-  otp: string;
+    otp: string;
 }
 
-export const OtpSignIn = ({otp} : OtpSignInProps) => {
-  return (
-    <Html>
-      Din engangskode er: {otp}
-    </Html>
-  );
-}
+export const OtpSignIn = ({ otp = "123456" }: OtpSignInProps) => {
+    return (
+        <Html>
+            <Head />
+            <Body style={main}>
+                <Container style={container}>
+                    <Img
+                        src={`${env.BASE_URL}/static/logomark.jpeg`}
+                        width="100"
+                        height="100"
+                        alt="TIHLDE Logomark"
+                        style={logo}
+                    />
+                    <Heading style={secondary}>
+                        Skriv inn følgende kode
+                        <br /> for å logge inn på TIHLDE
+                    </Heading>
+                    <Section style={codeContainer}>
+                        <Text style={code}>{otp}</Text>
+                    </Section>
+                    <Text style={paragraph}>
+                        Forventet du ikke denne e-posten?
+                    </Text>
+                    <Text style={paragraph}>
+                        Du kan trygt ignorere denne e-posten hvis du ikke prøvde
+                        å logge inn
+                    </Text>
+                </Container>
+                <Text style={footer}>Levert av INDEX</Text>
+            </Body>
+        </Html>
+    );
+};
 
 export default OtpSignIn;
+
+const main = {
+    backgroundColor: "#ffffff",
+    fontFamily: "HelveticaNeue,Helvetica,Arial,sans-serif",
+};
+
+const container = {
+    backgroundColor: "#ffffff",
+    border: "1px solid #eee",
+    borderRadius: "5px",
+    boxShadow: "0 5px 10px rgba(20,50,70,.2)",
+    marginTop: "20px",
+    maxWidth: "360px",
+    margin: "0 auto",
+    padding: "68px 0 130px",
+};
+
+const logo = {
+    margin: "0 auto",
+    marginBottom: "24px",
+};
+
+const tertiary = {
+    color: "#0a85ea",
+    fontSize: "11px",
+    fontWeight: 700,
+    fontFamily: "HelveticaNeue,Helvetica,Arial,sans-serif",
+    height: "16px",
+    letterSpacing: "0",
+    lineHeight: "16px",
+    margin: "16px 8px 8px 8px",
+    textTransform: "uppercase" as const,
+    textAlign: "center" as const,
+};
+
+const secondary = {
+    color: "#000",
+    display: "inline-block",
+    fontFamily: "HelveticaNeue-Medium,Helvetica,Arial,sans-serif",
+    fontSize: "20px",
+    fontWeight: 500,
+    lineHeight: "24px",
+    marginBottom: "0",
+    marginTop: "0",
+    width: "100%",
+    textAlign: "center" as const,
+};
+
+const codeContainer = {
+    background: "rgba(0,0,0,.05)",
+    borderRadius: "4px",
+    margin: "16px auto 14px",
+    verticalAlign: "middle",
+    width: "280px",
+};
+
+const code = {
+    color: "#000",
+    fontFamily: "HelveticaNeue-Bold",
+    fontSize: "32px",
+    fontWeight: 700,
+    letterSpacing: "6px",
+    lineHeight: "40px",
+    paddingBottom: "8px",
+    paddingTop: "8px",
+    margin: "0 auto",
+    display: "block",
+    textAlign: "center" as const,
+};
+
+const paragraph = {
+    color: "#444",
+    fontSize: "15px",
+    fontFamily: "HelveticaNeue,Helvetica,Arial,sans-serif",
+    letterSpacing: "0",
+    lineHeight: "23px",
+    padding: "0 40px",
+    margin: "0",
+    marginTop: "10px",
+    textAlign: "center" as const,
+};
+
+const footer = {
+    color: "#000",
+    fontSize: "12px",
+    fontWeight: 800,
+    letterSpacing: "0",
+    lineHeight: "23px",
+    margin: "0",
+    marginTop: "20px",
+    fontFamily: "HelveticaNeue,Helvetica,Arial,sans-serif",
+    textAlign: "center" as const,
+    textTransform: "uppercase" as const,
+};
