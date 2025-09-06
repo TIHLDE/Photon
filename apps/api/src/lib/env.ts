591a81fa0476f66bc7e64913b4f3cf137eb95a60
Mori <anders.morille@gmail.com>
2025-09-06 14:06:16 +0200

    update env vars

--- apps/api/src/lib/env.ts
+++ apps/api/src/lib/env.ts
@@ -6,6 +6,16 @@ const envSchema = z.object({
     FEIDE_CLIENT_ID: z.string({ description: "Feide OAuth Client ID" }),
     FEIDE_CLIENT_SECRET: z.string({ description: "Feide OAuth Client Secret" }),
     DATABASE_URL: z.string({ description: "Database URL" }),
+    BASE_URL: z
+        .string({ description: "Base URL of the application" })
+        .default("http://localhost:4000"),
+    SEED_DB: z
+        .string({
+            description:
+                "If the database is empty on start, will seed the database. Turned off by default",
+        })
+        .optional()
+        .transform((val) => val === "true" || val === "1"),
 });
 
 /**
