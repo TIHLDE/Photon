8019489ee38924e2647259b4500cdcf360fae4b3
Mori <anders.morille@gmail.com>
2025-10-27 20:03:02 +0100

    Added migrations

---
+++ drizzle/0002_heavy_triton.sql
@@ -0,0 +1,12 @@
+CREATE TABLE "notification_notification" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"user_id" text NOT NULL,
+	"title" text NOT NULL,
+	"description" text NOT NULL,
+	"link" text,
+	"is_read" boolean DEFAULT false NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+ALTER TABLE "notification_notification" ADD CONSTRAINT "notification_notification_user_id_auth_user_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."auth_user"("id") ON DELETE cascade ON UPDATE no action;
