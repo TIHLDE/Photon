0f702f7444a6ec8564d66bb08918c7626d5fbcd9
= <anders.morille@gmail.com>
2026-01-14 22:49:01 +0100

    Add username for seeded test user

--- src/db/seed/auth.ts
+++ src/db/seed/auth.ts
@@ -20,6 +20,9 @@ export default async ({ db, auth }: AppContext) => {
                 password: "index123",
                 name: "Brotherman Testern",
                 role: "admin",
+                data: {
+                    username: "brotherman",
+                },
             },
         });
 
