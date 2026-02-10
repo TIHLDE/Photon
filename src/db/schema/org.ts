3d14a8cd10bd8c5556bbc30cdec8e3b6198d6b67
Anders Morille <anders.morille@gmail.com>
2026-02-10 10:39:30 +0100

    Linting

--- src/db/schema/org.ts
+++ src/db/schema/org.ts
@@ -1,3 +1,4 @@
+import { relations } from "drizzle-orm";
 import {
     boolean,
     integer,
@@ -13,7 +14,6 @@ import { pgEnum } from "drizzle-orm/pg-core";
 import { timestamps } from "../timestamps";
 import { user } from "./auth";
 import { role } from "./rbac";
-import { relations } from "drizzle-orm";
 
 const pgTable = pgTableCreator((name) => `org_${name}`);
 
