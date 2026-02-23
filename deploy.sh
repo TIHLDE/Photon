ec6a2a365d3b9966f0fdb0e373a1b387ee3dadf1
Borgar Barland <142639383+borgaar@users.noreply.github.com>
2026-02-23 23:53:08 +0100

    migrate to dockerized redis

--- deploy.sh
+++ deploy.sh
@@ -13,8 +13,14 @@ IMAGE_NAME="$DOMAIN:$COMMIT_HASH"
 echo "-> Building new Docker image"
 docker build --no-cache -f infra/docker/Dockerfile -t $IMAGE_NAME .
 
-echo "-> Stopping and removing old container"
+echo "-> Stopping and removing old Photon container"
 docker rm -f $DOMAIN || true
 
-echo "-> Starting new container"
+echo "-> Stopping and removing old Redis container"
+docker rm -f $DOMAIN-redis || true
+
+echo "-> Starting new Redis container"
+docker run -d --pull always --name $DOMAIN-redis --restart unless-stopped redis:alpine
+
+echo "-> Starting new Photon container"
 docker run --env-file $ENV_FILE_PATH -p $IP_ADDRESS:$PORT:4000 --name $DOMAIN --restart unless-stopped -d $IMAGE_NAME
