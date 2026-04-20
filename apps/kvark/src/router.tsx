80330a0bd819c815f724d526ec9780874d3e1f2b
Aleksander Evensen <ahse03@gmail.com>
2026-04-20 17:46:00 +0200

    initial commit

---
+++ apps/kvark/src/router.tsx
@@ -0,0 +1,31 @@
+import { createRouter as createTanStackRouter } from "@tanstack/react-router";
+import { routeTree } from "./routeTree.gen";
+
+import { QueryClientProvider } from "@tanstack/react-query";
+import { setupRouterSsrQueryIntegration } from "@tanstack/react-router-ssr-query";
+import { getContext } from "./integrations/tanstack-query/client";
+
+export function getRouter() {
+  const context = getContext();
+
+  const router = createTanStackRouter({
+    routeTree,
+    context,
+    scrollRestoration: true,
+    defaultPreload: "intent",
+    defaultPreloadStaleTime: 0,
+    Wrap({ children }) {
+      return <QueryClientProvider client={context.queryClient}>{children}</QueryClientProvider>;
+    },
+  });
+
+  setupRouterSsrQueryIntegration({ router, queryClient: context.queryClient });
+
+  return router;
+}
+
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: ReturnType<typeof getRouter>;
+  }
+}
